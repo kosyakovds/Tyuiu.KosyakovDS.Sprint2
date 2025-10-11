2d5befbbb457dac6535c654deb1b68074860f82c
admin <admin@DESKTOP-KB5HODH>
2025-10-11 10:25:41 +0300

    Add project files.

---
+++ Tyuiu.KosyakovDS.Sprint2.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KosyakovDS.Sprint2.Task0.V2.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint2.Task0.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            DataService ds = new DataService();
+            int x = 123;
+            int y = 123;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { false, true, false, true, false, true };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KosyakovDS.Sprint2.Task0.V2/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.KosyakovDS.Sprint2.Task0.V2.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint2.Task0.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил Косяков Д. С. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #2                                                             *");
+            Console.WriteLine("* Выполнил: Косяков Дмитрий Сергеевич | ИИПб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений и арифметических выражений,    *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив):                   *");
+            Console.WriteLine("* (False, True, False, True, False, True), при x = 123, y = 123           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* x = 123, y = 123                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите x:");
+            int x = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите y:");
+            int y = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(ds.GetCompareOperations(x, y)[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
